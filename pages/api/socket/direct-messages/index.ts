0b01c3110853d3bd67706e901fd54e5d9f060e12
Zaid <mohmmadzaid759@gmail.com>
2025-07-27 02:55:47 +0530

    Bug fix in  or

--- pages/api/socket/direct-messages/index.ts
+++ pages/api/socket/direct-messages/index.ts
@@ -1,91 +1,82 @@
-import { NextApiRequest } from "next";
 import { NextApiResponseServerIo } from "@/app/type";
 import currentProfilePageRouter from "@/lib/current-profile-page";
 import db from "@/lib/db";
+
+import { NextApiRequest } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponseServerIo
 ) {
-  if (req.method === "POST") {
-    try {
-      const user = await currentProfilePageRouter(req);
-      if (!user) {
-        res.status(401).json({ message: "Unauthorized" });
-        return;
-      }
-      const { conversationId } = req.query;
-      const { content, fileUrl } = req.body;
-      if (!conversationId) {
-        return res.status(400).json({ message: "Missing parametes" });
-      }
-      if (!content && !fileUrl) {
-        return res
-          .status(400)
-          .json({ message: "Content or fileUrl is required" });
-      }
-      const conversation = await db.conversation.findFirst({
-        where: {
-          id: conversationId as string,
-          OR: [
-            {
-              memberOne: {
-                profileId: user.id,
-              },
-              memberTwo: {
-                profileId: user.id,
-              },
-            },
-          ],
-        },
-        include: {
-          memberOne: {
-            include: {
-              profile: true,
-            },
-          },
-          memberTwo: {
-            include: {
-              profile: true,
-            },
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const profile = await currentProfilePageRouter(req);
+    const { content, fileUrl } = req.body;
+    const { conversationId } = req.query;
+
+    if (!profile) return res.status(401).json({ error: "Unauth" });
+
+    if (!conversationId)
+      return res.status(400).json({ error: "Conversation Id Missing" });
+
+    if (!content) return res.status(400).json({ error: "Content missing" });
+
+    const conversation = await db.conversation.findFirst({
+      where: {
+        id: conversationId as string,
+        OR: [
+          { memberOne: { profileId: profile.id } },
+          { memberTwo: { profileId: profile.id } },
+        ],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
           },
         },
-      });
-      if (!conversation) {
-        return res.status(404).json({ message: "Conversation not found" });
-      }
-      const senderId =
-        conversation.memberOne.profileId === user.id
-          ? conversation.memberOne.profileId
-          : conversation.memberTwo.profileId;
-      if (!senderId)
-        return res.status(404).json({ message: "Member not found" });
-      const message = await db.directMessage.create({
-        data: {
-          content,
-          fileUrl,
-          memberId: senderId,
-          conversationId: conversationId as string,
-        },
-        include: {
-          member: {
-            include: {
-              profile: true,
-            },
+        memberTwo: {
+          include: {
+            profile: true,
           },
         },
-      });
-      const channelKey = `chat:${conversationId}:messages`;
-      if (res?.socket?.server?.io) {
-        console.log("socket is connect on backend add");
-      }
-      res?.socket?.server?.io?.emit(channelKey, message);
-      return res.status(200).json(message);
-    } catch (error) {
-      console.log("Messages_POST", error);
-      return res.status(500).json({ message: "Internal error" });
-    }
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+      },
+    });
+
+    if (!conversation)
+      return res.status(404).json({ message: "Conversation not found" });
+
+    const member =
+      conversation.memberOne.profileId === profile.id
+        ? conversation.memberOne
+        : conversation.memberTwo;
+
+    if (!member) return res.status(404).json({ message: "Member not found" });
+
+    const message = await db.directMessage.create({
+      data: {
+        content,
+        fileUrl,
+        conversationId: conversationId as string,
+        memberId: member.id,
+      },
+      include: {
+        member: {
+          include: { profile: true },
+        },
+      },
+    });
+
+    const channelKey = `chat:${conversationId}:messages`;
+
+    res?.socket?.server?.io?.emit(channelKey, message);
+
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("Direct Messages_POST", error);
+    return res.status(500).json({ message: "Internal error" });
   }
 }
